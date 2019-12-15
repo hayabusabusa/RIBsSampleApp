d49f05a5bcb3daec6acec241ea74475425f76b7f
hayabusabusa <31949692+hayabusabusa@users.noreply.github.com>
2019-12-16 01:54:59 +0900

    Add project

---
+++ RIBsSampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RIBsSampleApp
+//
+//  Created by 山田隼也 on 2019/12/15.
+//  Copyright © 2019 Shunya Yamada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
