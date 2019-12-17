b2b8e771899c47dd58928bb81edf3d634f9f28b3
hayabusabusa <hayabusabusaSY@gmail.com>
2019-12-17 14:30:39 +0900

    Add base view controller

---
+++ RIBsSampleApp/View/Base/BaseViewController.swift
@@ -0,0 +1,15 @@
+//
+//  BaseViewController.swift
+//  RIBsSampleApp
+//
+//  Created by Yamada Shunya on 2019/12/17.
+//  Copyright © 2019 Shunya Yamada. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+
+class BaseViewController: UIViewController {
+    
+    let disposeBag = DisposeBag()
+}
